30d5f9c60d550ba3f8bc96d5efa03a60e6893609
nalualvarez <analuisaalvarez@cjr.org.br>
2016-06-21 13:45:11 -0300

    comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
   get 'welcome/index'
-  resources :articles
+
+  resources :articles do
+    resources :comments
+    
+  end
+
+
 
  
   # The priority is based upon order of creation: first created -> highest priority.
